64ecc2dfb79c3c9a506874d3d30033a78f467dd9
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2021-12-20 11:49:06 +0300

    Initial commit

---
+++ src/main/java/lesson_2/Main.java
@@ -0,0 +1,19 @@
+package lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        //int a = 1 / 0;
+        test();
+    }
+
+    public static void test() {
+        test1();
+    }
+
+    public static void test1() {
+        test2();
+    }
+    public static void test2() {
+        int a = 1 / 0;
+    }
+}
