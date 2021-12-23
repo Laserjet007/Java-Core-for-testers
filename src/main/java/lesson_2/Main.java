74ffde12e84b87c0eafbc6a31493d359a4c2831e
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2021-12-23 12:51:03 +0300

    3

--- src/main/java/lesson_2/Main.java
+++ src/main/java/lesson_2/Main.java
@@ -4,17 +4,30 @@ public class Main {
     public static void main(String[] args) {
         //int a = 1 / 0;
         test();
+        test1();
+        test2();
+        System.out.println("method main");
     }
 
     public static void test() {
+        //test();
         test1();
+        System.out.println("method test");
     }
 
     public static void test1() {
         test2();
+        System.out.println("method test1");
     }
     public static void test2() {
-        int a = 1 / 0;
+       // int a = 1 / 0;
+
+        /*StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        for (StackTraceElement stackTraceElement : stackTraceElements) {
+            System.out.println(stackTraceElement.getMethodName() + " " + stackTraceElement.getLineNumber());
+        }*/
+
+        System.out.println("method test2");
     }
-    System.out.println("rrr")
+
 }
---
+++ src/main/java/lesson_3/BoxWithTwoGenerics.java
@@ -0,0 +1,6 @@
+package lesson_3;
+
+public class BoxWithTwoGenerics<T, S> {
+    private T obj1;
+    private S obj1;
+}
