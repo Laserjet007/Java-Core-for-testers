5ccf66f5fb748a14b4e2b4205226bcbd5bd5a22c
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-05 14:12:39 +0300

    Collections

--- src/main/java/lesson_5/StreamExamples.java
+++ src/main/java/lesson_5/StreamExamples.java
@@ -1,4 +1,13 @@
 package lesson_5;
 
+import java.util.Arrays;
+
 public class StreamExamples {
+    public static void main(String[] args) {
+        String tempString = "кот";
+        System.out.println((Arrays.toString(tempString.getBytes())));//вывести чистовой код слова кот в консоль
+
+            String tempString1 = "cat";
+            System.out.println((Arrays.toString(tempString.getBytes())));//вывести чистовой код слова cat в консоль
+    }
 }
