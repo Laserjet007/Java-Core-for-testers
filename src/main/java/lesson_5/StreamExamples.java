0e5e469f919346d49a5e09245b94efb0a6d37c6b
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-06 13:40:23 +0300

    Collections

--- src/main/java/lesson_5/StreamExamples.java
+++ src/main/java/lesson_5/StreamExamples.java
@@ -1,5 +1,7 @@
 package lesson_5;
 
+import java.io.*;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 public class StreamExamples {
@@ -7,7 +9,20 @@ public class StreamExamples {
         String tempString = "кот";
         System.out.println((Arrays.toString(tempString.getBytes())));//вывести чистовой код слова кот в консоль
 
-            String tempString1 = "cat";
-            System.out.println((Arrays.toString(tempString.getBytes())));//вывести чистовой код слова cat в консоль
+        String tempString1 = "cat";
+        System.out.println((Arrays.toString(tempString.getBytes())));//вывести чистовой код слова cat в консоль
+
+        System.out.println(Charset.defaultCharset());//вывести кодировку по умолчанию
+        System.out.println(Charset.availableCharsets());// вывести имеющиеся кодировки
+
+        File file = new File("our_example.txt"); // создаем файл
+
+       try (OutputStream stream = new FileOutputStream(file)){// можно в новой версии джавы обьявить поток а затем его и открыть
+
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
     }
 }
