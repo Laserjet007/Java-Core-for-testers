da182dcf9e40f2eae80d716e79ebe27e2bb1a272
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-07 11:31:15 +0300

    Collections

--- src/main/java/lesson_5/StreamExamples.java
+++ src/main/java/lesson_5/StreamExamples.java
@@ -2,6 +2,7 @@ package lesson_5;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class StreamExamples {
@@ -17,18 +18,60 @@ public class StreamExamples {
 
         File file = new File("our_example.txt"); // создаем файл
 
-        OutputStream stream = null;//вариант закрытия программы с обычным траем, без библиотек джавы
-        try {// обработка исключений
+        /*OutputStream stream = null;//вариант закрытия программы с обычным траем, без библиотек джавы
+        try {                        // трай кетч - обработка исключений
             stream = new FileOutputStream(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
+        } finally {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }*/
 
-       /*try (OutputStream stream = new FileOutputStream(file)){// можно в новой версии джавы обьявить поток а затем его и открыть
+       try (OutputStream outputStream = new FileOutputStream(file)){// ресурсный трай кетч, можно в новой версии джавы обьявить поток а затем его и открыть
+       outputStream.write("кот".getBytes(StandardCharsets.UTF_8));//добавить запись из байт в файл
+
        } catch (FileNotFoundException e) {//для перекрытия потка необходимо его закрывать так как может подсасывать память или вообще блокернуть файл
            e.printStackTrace();
        } catch (IOException e) {
-           e.printStackTrace();
-       }*/
+           e.printStackTrace();// с этим вариантом запись прекратится и запишит следующую хрень, а то может начать гонять циклом
+       }
+
+        /*try (OutputStream outputStream1 = new FileOutputStream(file)) {//открываем еще один стрим путь к этому же файлу
+        outputStream1.write(65);//передадим инт -переменную (код буквы а)
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        try (OutputStream outputStream1 = new FileOutputStream(file, true)) {//вариант - когда слово будет дописываться в конец
+            outputStream1.write(65);                                     //append true - записать в конец
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        long time = System.currentTimeMillis();//замерить время записи большого файла 5 мб
+
+        try (OutputStream outputStream1 = new FileOutputStream(file, true)) {
+            for (int i = 0; i < 5 * 1024 * 1024; i++) { // от 0 до 1000000 ( i меньше 0 и меньше 5 умножить на килобайт и еще раз на килобайт
+                outputStream1.write(65);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("время затраченное на запись 5 мг" + (System.currentTimeMillis() - time));
+
+
+        long time1 = System.currentTimeMillis();//замерить время записи большого файла 5 мб (второй способ быстрее передать файл в поток)
     }
 }
