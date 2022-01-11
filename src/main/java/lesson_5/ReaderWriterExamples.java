bb2740d634b3d8466dc84f12638f8e65936d69c6
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-11 13:08:57 +0300

    Collections

--- src/main/java/lesson_5/ReaderWriterExamples.java
+++ src/main/java/lesson_5/ReaderWriterExamples.java
@@ -2,21 +2,31 @@ package lesson_5;
 
 //символьные потоки
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class ReaderWriterExamples {
-    public static void main(String[] args) {     //точка входа
-        try {InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("1.txt"), StandardCharsets.UTF_8);//поточный , но заврнутый в символьную обработку (в ковычки передаем поток new FileInputStream)/ можно еще и указать кодировку
+    public static void main(String[] args) throws FileNotFoundException {     //точка входа
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("1.txt"), StandardCharsets.UTF_8);//поточный , но заврнутый в символьную обработку (в ковычки передаем поток new FileInputStream)/ можно еще и указать кодировку
             int x;//читать в цыкле каждый символ
-            while ((x = inputStreamReader.read()) != -1); {//считывается новый символ и присваивается х... читать до - 1 означает коннец файла
+            while ((x = inputStreamReader.read()) != -1) ;
+            {//считывается новый символ и присваивается х... читать до - 1 означает коннец файла
                 System.out.println(x);//выводим символ в консоль
                 System.out.println((char) x);//преобразовываем в букву
             }
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // пример чтения и записи через буфер целиком построчно
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("1.txt"))) {
+            System.out.println(bufferedReader.readLine()); //прочитать строчку и вывести
+
+        } catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
