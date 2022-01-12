a70cbf2e59d8255132d6f21ad6e378a213cf64c8
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-12 14:57:56 +0300

    Collections

--- src/main/java/lesson_5/SerializationExamples.java
+++ src/main/java/lesson_5/SerializationExamples.java
@@ -2,10 +2,27 @@ package lesson_5;
 
 //вариант передачи обьекта ссылочного типа ( не строка или буквы ) или вариант передачи обьекта джавы что бы он лежал на выходе для работы с ним
 
-import java.io.File;
+import java.io.*;
 
 public class SerializationExamples {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File file = new File("student");//открываем файт в который будем сериализовывать
+
+        Student student = new Student("Anton", "Gangonov", 45);
+        Student student1 = new Student("Ivan", "Heronov", 76);
+        Student student2 = new Student("Semen", "Rudicov", 98);
+        Student student3 = new Student("Drtg", "Dthhyjj", 45);
+
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {; //записываем обьекты-студенты в файл (можно также по сети передать
+            objectOutputStream.writeObject(student);//вызываем целиком обьект
+            objectOutputStream.writeObject(student1);
+            objectOutputStream.writeObject(student2);
+            objectOutputStream.writeObject(student3);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
