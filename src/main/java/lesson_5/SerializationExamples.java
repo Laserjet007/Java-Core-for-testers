87f8df33587a5994b3ed1224528f176fd70dd08e
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-12 13:25:07 +0300

    Collections

--- src/main/java/lesson_5/SerializationExamples.java
+++ src/main/java/lesson_5/SerializationExamples.java
@@ -2,5 +2,10 @@ package lesson_5;
 
 //вариант передачи обьекта ссылочного типа ( не строка или буквы ) или вариант передачи обьекта джавы что бы он лежал на выходе для работы с ним
 
+import java.io.File;
+
 public class SerializationExamples {
+    public static void main(String[] args) {
+        File file = new File("student");//открываем файт в который будем сериализовывать
+    }
 }
