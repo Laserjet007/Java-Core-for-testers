5793cf727ef9de0e8a822c77cf47eb89119037a9
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-17 10:53:59 +0300

    Collections

--- src/main/java/lesson_6/URLExamples.java
+++ src/main/java/lesson_6/URLExamples.java
@@ -9,6 +9,9 @@ public class URLExamples {
     public static void main(String[] args) throws MalformedURLException {
         URL url = new URL("https://smirnov:password@geekbrains.ru:80/curses?type=qa&level=super#1");// обертка для сокета - класс урл. и в конструктор нужно передать адрес . передать дополнительные параметры можно через знак ? после идет параметр и от если нуно много параметров отделяем амперсантом (#1 -вернуться к началу где читали. якорь) (smirnov:password - вызов аутинтификации)
 
+        System.out.println(url.getAuthority());//этот гет вернет авторизацию (логин пароль)
+        System.out.println(url.getPort();//протокол
+        System.out.println(url.getHost());//выводим хост и порт
 
 
 
