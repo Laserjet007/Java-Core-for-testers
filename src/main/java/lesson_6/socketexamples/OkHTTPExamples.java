a4c4717b55a53573e50d9c1a7bb52480bc214c7e
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-17 23:13:57 +0300

    Collections

--- src/main/java/lesson_6/socketexamples/OkHTTPExamples.java
+++ src/main/java/lesson_6/socketexamples/OkHTTPExamples.java
@@ -2,8 +2,10 @@ package lesson_6.socketexamples;
 
 //соединение с помощью библиотеки
 
+import okhttp3.OkHttpClient;
+
 public class OkHTTPExamples {
     public static void main(String[] args) {
-
+        OkHttpClient okHttpClient = new OkHttpClient();
     }
 }
