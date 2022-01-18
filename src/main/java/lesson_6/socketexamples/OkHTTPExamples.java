fbb10b81f97379283d40c91edf04f305754bca33
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-18 13:35:00 +0300

    Collections

--- src/main/java/lesson_6/socketexamples/OkHTTPExamples.java
+++ src/main/java/lesson_6/socketexamples/OkHTTPExamples.java
@@ -2,10 +2,44 @@ package lesson_6.socketexamples;
 
 //соединение с помощью библиотеки
 
+/*import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+import static java.nio.file.attribute.AclEntry.newBuilder;
 
 public class OkHTTPExamples {
-    public static void main(String[] args) {
-        OkHttpClient okHttpClient = new OkHttpClient();
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient();// в конструкторе используется патерное проектирование (шаблоны по которым оптимально писать приложения)
+                 .newBuilder()             //шаблон билдер (работает с сеторами)
+                 .readTimeout()
+                 .build();                         //  после ввода параметров означает что обьект построен
+
+        HttpUrl url = new HttpUrl.Builder()         //вместо newBuilder() можно сделать так
+                .scheme("https")              //протокол называется схема
+                .host("gb.ru")
+                .addPathSegment("/courses")    //сигмент пути ( как в предидущих соединениях / или патч)
+                .build();
+
+       Request request = new Request.Builder()     //формируем сам запрос
+               .url(url)                 //передаем адрес
+               .get()//передаем гет запрос
+               .build();
+
+       Response response = okHttpClient.newCall(request).execute();//тветет получаем из клиента
+
+        System.out.println(response.code()); // код ответа
+        System.out.println(response.headers());
+
+        System.out.println(response.body().string());
     }
+
+
+
+
 }
+
+*/
