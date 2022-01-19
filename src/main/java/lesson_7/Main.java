be7c8442df5b035068c60b3f6d69f9885caf31f0
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-19 12:07:47 +0300

    пишим клиенскую часть приложения которая обращается к серверу и получает с него данные и их отображать

---
+++ src/main/java/lesson_7/Main.java
@@ -0,0 +1,14 @@
+package lesson_7;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//пишим клиенскую часть приложения которая обращается к серверу и получает с него данные и их отображать
+//добавляем библиотеку jackson. в мавен
+public class Main {
+    public static void main(String[] args) {
+       ObjectMapper objectMapper = new  ObjectMapper();//класс ObjectMapper позволяет сравнивать данные что есть в джейсоне с тем с чем работаем в джаве
+
+
+
+    }
+}
