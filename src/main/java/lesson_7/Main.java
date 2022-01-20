fe0a2368868120689d6004d2aac57af2eec579b9
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-20 08:47:30 +0300

    переделываем обьект кар в джейсон обьект

--- src/main/java/lesson_7/Main.java
+++ src/main/java/lesson_7/Main.java
@@ -8,13 +8,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
        ObjectMapper objectMapper = new  ObjectMapper();  //класс ObjectMapper позволяет сравнивать данные что есть в джейсоне с тем с чем работаем в джаве
-
+// переделываем обьект кар в джейсон обьект c помощью objectMapper
         Car car = new Car("Red", "Porsche");
-        String carFromJSON = objectMapper.writeValueAsString(car);  // переделываем обьект кар в джейсон обьект c помощью objectMapper
+        String carFromJSON = objectMapper.writeValueAsString(car);
         System.out.println(carFromJSON);
 
+//проделать перидущую операцию и собрать обьект из джейсона
+        Car car1 = objectMapper.readValue(carFromJSON, Car.class);
+        System.out.println(car1); //из за реализации ту стринг из маин - пишим просто кар1
 
-        //проделать перидущую операцию и собрать обьект из джейсона
+       // ВАРИАНТ НЕ СО СТРОКОЙ КАК В ПРЕДИДУЩЕМ ВАРИАНТЕ - А С СПИСКОМ (вариант посложнее)
+
 
     }
 }
