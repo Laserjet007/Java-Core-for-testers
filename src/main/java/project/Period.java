4847c54942c8f5c9bf5d4efd76ad2c98cd5f6989
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-01-27 15:19:30 +0300

    1

---
+++ src/main/java/project/Period.java
@@ -0,0 +1,5 @@
+package project;
+
+public enum Period { //enum перечисление
+    NOW, FIVE_DAYS
+}
---
+++ src/main/java/project/WeatherModel.java
@@ -0,0 +1,6 @@
+package project;
+import java.io.IOException;
+
+public interface WeatherModel {  // что бы наследники реализовывали, добавляю метод гет ведер, готорый будет получать погоду с того или иного сервиса
+    void getWeather(String selectedCity, Period period) throws IOException;// метод будет получать и адресовывать на интерфес ( с двумя параметрами : город и период
+}
